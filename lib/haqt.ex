80817cae823bcd51ec78f17488d87f0e1cd8c5c8
Todd Pickell <tapickell@gmail.com>
2017-03-07 17:04:23 -0600

    initial commit

---
+++ lib/haqt.ex
@@ -0,0 +1,18 @@
+defmodule Haqt do
+  @moduledoc """
+  Documentation for Haqt.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Haqt.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/haqt_test.exs
@@ -0,0 +1,8 @@
+defmodule HaqtTest do
+  use ExUnit.Case
+  doctest Haqt
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
