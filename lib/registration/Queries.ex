afe4fa9100ee94fac635de600d20a217d16422c9
Todd Pickell <tapickell@gmail.com>
2017-03-09 22:52:25 -0600

    Adds the rest of the functions needed to queries
    This is all that should be needed for now, their should be some
    validation on storing data that it does have the proper keys
    somewhere.
    
    Todd Pickell <tapickell@gmail.com> @tapickell

--- lib/registration/Queries.ex
+++ lib/registration/Queries.ex
@@ -1,22 +1,33 @@
 defmodule Haqt.Registration.Queries do
+  alias Haqt.Registration.GenServerStore
+
+  # error handle around type key
   def number_of_attendees do
-    registrations
-    |> Enum.filter(fn(reg) -> reg.registration_type == "attendee" end)
+    registrations()
+    |> Enum.filter(fn(reg) -> reg.type == "attendee" end)
     |> Enum.count
   end
 
   def number_of_speakers do
-    registrations
-    |> Enum.filter(fn(reg) -> reg.registration_type == "speaker" end)
+    registrations()
+    |> Enum.filter(fn(reg) -> reg.type == "speaker" end)
     |> Enum.count
   end
 
   def number_of_registrations do
-    registrations
+    registrations()
     |> Enum.count
   end
 
+  def register(registration) do
+    GenServerStore.register(Registrations, registration)
+  end
+
+  def unregister(registration) do
+    GenServerStore.unregister(Registrations, registration)
+  end
+
   defp registrations do
-    Haqt.Registration.GenServerStore.registrations(Registrations)
+    GenServerStore.registrations(Registrations)
   end
 end
